2f346ffed24ee496825cef4e03b8286a9ffa6afb
tnarayanan <tejasn100@gmail.com>
2017-09-07 18:44:30 -0700

    Updated Template Name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateTeleOp.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Template: Linear OpMode", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+@TeleOp(name="Template Tele-Op", group="Linear Opmode")  // @Autonomous(...) is the other common choice
 @Disabled
 public class TemplateTeleOp extends LinearOpMode {
 
