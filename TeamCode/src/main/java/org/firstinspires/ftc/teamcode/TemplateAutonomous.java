d56262ff82155d9a8684e1b6c857c619c8c7da76
tnarayanan <tejasn100@gmail.com>
2017-09-07 18:39:19 -0700

    Updated SDK

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateAutonomous.java
@@ -32,15 +32,8 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 package org.firstinspires.ftc.teamcode;
 
-import android.graphics.Color;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
@@ -56,7 +49,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="7128 First Auto", group="Autonomous")
+@Autonomous(name="TemplateAutonomous", group="Autonomous")
 
 public class TemplateAutonomous extends LinearOpMode {
 
