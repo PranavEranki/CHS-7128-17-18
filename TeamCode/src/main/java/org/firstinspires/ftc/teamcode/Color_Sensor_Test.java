e578e6a67f9890faac14c8b515de16d27739d5c2
eperiyasamy <rithue2000@gmail.com>
2017-10-19 16:13:23 -0700

    Color Sensor Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Sensor_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color_Sensor_Test.java
@@ -88,9 +88,9 @@ public class Color_Sensor_Test extends LinearOpMode {
             telemetry.addData("Color", "Red: " + colorSensor.red());
             telemetry.addData("Color", "Green: " + colorSensor.green());
             telemetry.addData("Color", "Blue: " + colorSensor.blue());
-            if(colorSensor.red() > 100 && colorSensor.red() < 200 && colorSensor.blue() > 100 && colorSensor.blue() < 200 && colorSensor.green() > 100 && colorSensor.green() < 200 ){
+            if(colorSensor.red() > 0 && colorSensor.red() < 100 && colorSensor.blue() > 0 && colorSensor.blue() < 100 && colorSensor.green() > 0 && colorSensor.green() < 100 ){
                 telemetry.addData("Color", "Color: Brown");
-            }else if(colorSensor.red() > 200 && colorSensor.red() < 300 && colorSensor.blue() > 200 && colorSensor.blue() < 300 && colorSensor.green() > 200 && colorSensor.green() < 300){
+            }else if(colorSensor.red() > 100 && colorSensor.red() < 200 && colorSensor.blue() > 90 && colorSensor.blue() < 200 && colorSensor.green() > 100 && colorSensor.green() < 200){
                 telemetry.addData("Color", "Color: Gray");
             }else{
                 telemetry.addData("Color", "Color: None");
